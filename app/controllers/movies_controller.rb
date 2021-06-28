100e1f2e9597bcfb99c0caa907b2acd94fecee02
Pippinu <iacono760@gmail.com>
2021-06-28 15:22:34 +0200

    Changes that i dont remember

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,16 @@ class MoviesController < ApplicationController
     def index
       @movies = Movie.all
     end
+
+    def PGFilms
+      @pgFilms = Movie.where("rating = 'PG'")
+    end
+
+    def RFilms
+      @rFilms = Movie.where("rating = 'R'")
+    end
+
+    def searchFilm(filmToSearch)
+      @filmToSearch = Movie.where("title = '#{filmToSearch}'")
+    end
 end
