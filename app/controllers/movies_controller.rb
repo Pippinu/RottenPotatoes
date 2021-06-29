975527548afff0a57b553055da6f8e2db61982ce
Pippinu <iacono760@gmail.com>
2021-06-29 11:13:48 +0200

    per ora worka

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,15 +3,9 @@ class MoviesController < ApplicationController
       @movies = Movie.all
     end
 
-    def PGFilms
-      @pgFilms = Movie.where("rating = 'PG'")
-    end
-
-    def RFilms
-      @rFilms = Movie.where("rating = 'R'")
-    end
-
-    def searchFilm(filmToSearch)
-      @filmToSearch = Movie.where("title = '#{filmToSearch}'")
+    def show
+      id = params[:id] # retrieve movie ID from URI route
+      @movie = Movie.find(id) # look up movie by unique ID
+      # will render app/views/movies/show.html.haml by default
     end
 end
